d7222e75420b1c08cf103be51ede5623f83e2c3b
Ileolami <99335756+Ileolami@users.noreply.github.com>
2026-03-25 12:29:29 +0100

    Using typescript

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,20 +1,19 @@
+import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-
-/** @type import('hardhat/config').HardhatUserConfig */
-export const solidity = "0.8.28";
-
-const { vars } = require("hardhat/config");
+import { vars } from "hardhat/config";
 
 const ACCESS_TOKEN = vars.get("ACCESS_TOKEN");
-
 const PRIVATE_KEY = vars.get("PRIVATE_KEY");
 
-module.exports = {
+const config: HardhatUserConfig = {
   solidity: "0.8.28",
   networks: {
-    sepolia: {
+    rskTestnet: {
       url: `https://go.getblock.io/${ACCESS_TOKEN}`,
       accounts: [PRIVATE_KEY],
+      chainId: 31,
     },
   },
 };
+
+export default config;
